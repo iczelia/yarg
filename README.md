5822120eb2e764144e4e97859e37625a4fd644ac
Kamila Szewczyk <k@iczelia.net>
2026-08-12 14:53:50 +0200

    style

--- README.md
+++ README.md
@@ -1,12 +1,13 @@
 # yarg
-Yet another UNIX-like argument parser for C. 0BSD-licensed.
+
+Yet another UNIX-like argument parser for C99. 0BSD-licensed.
 
 ## Why another argument parsing library?
 
 Four reasons:
-- `yarg` is a header-only library, meaning that you can simply copy-and-paste it into project. It is also CC0-licensed, meaning that you can use it in just about any program without licensing obligations.
+- `yarg` is a header-only library, meaning that you can simply copy-and-paste it into project. It is also 0BSD-licensed, meaning that you can use it in just about any program without complicated or undesirable licensing obligations.
 - `yarg` supports short and long options, Windows-style flags `/like-this` and short flag styles `-like -this=3`, not just the UNIX-y `-ab5 --like-this`.
 - `yarg` actually parses the arguments and gives you a structure that is fundamentally independent of the contents of argv.
 - `yarg` supports mixed positional arguments with flags, e.g. `--flag file_name.txt --flag2`.
 
-Despite being more featured than argument parsers like `getopt` or `getopt_long`, `yarg` retains a relatively small code footprint (200 LoC).
+Despite being more featured than argument parsers like `getopt` or `getopt_long`, `yarg` retains a relatively small code footprint (350 LoC).
--- demo.c
+++ demo.c
@@ -1,8 +1,11 @@
+/*  Written by Kamila Szewczyk (k@iczelia.net), released to
+    the public domain (0BSD).  */
 
 #include <stdio.h>
 #include "yarg.h"
 
-// ./a.out -ab5 pos-arg -v -c 5 --stuff --thing=3 --thing2 5 -- -name
+/*  Example invocation:
+    $ ./a.out -ab5 pos-arg -v -c 5 --stuff --thing=3 --thing2 5 -- -name  */
 int main(int argc, char * argv[]) {
   enum { LONG_ARG = CHAR_MAX + 1, STUFF, THING, THING2 };
   yarg_options opt[] = {
@@ -17,30 +20,20 @@ int main(int argc, char * argv[]) {
     { THING2,   optional_argument, "thing2" },
     {        0, no_argument,       NULL }
   };
-  yarg_settings settings = {
-    .dash_dash = true,
-    .style = YARG_STYLE_UNIX,
-  };
-
+  yarg_settings settings = { .dash_dash = true, .style = YARG_STYLE_UNIX };
   yarg_result * res = yarg_parse(argc, argv, opt, settings);
-  if (res->error) {
-    fputs(res->error, stderr);
-    yarg_destroy(res);
-    return 1;
-  }
+  if (res->error)
+    { fputs(res->error, stderr);  yarg_destroy(res);  return 1; }
   for (int i = 0; i < res->argc; i++) {
-    if (res->args[i].opt < CHAR_MAX) {
+    if (res->args[i].opt < CHAR_MAX)
       printf("Option: %c\n", res->args[i].opt);
-    } else {
+    else
       printf("Option: `%s'\n", res->args[i].long_opt);
-    }
-    if (res->args[i].arg) {
+  
+    if (res->args[i].arg)
       printf("Argument: %s\n", res->args[i].arg);
-    }
   }
-  for (int i = 0; i < res->pos_argc; i++) {
+  for (int i = 0; i < res->pos_argc; i++)
     printf("Positional argument: %s\n", res->pos_args[i]);
-  }
   yarg_destroy(res);
-  return 0;
 }
